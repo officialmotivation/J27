77fd434d7ca427af37fc5b149081e3c4bb7dd9c8
Home PC <officialmotivation72@gmail.com>
2026-05-27 15:17:24 +0530

    Configured

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"]
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  base: "/J27/",
 })
