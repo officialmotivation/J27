1cf31d9167268807dd2534794c0822f5ae210d00
officialmotivation <officialmotivation72@gmail.com>
2026-05-28 07:04:08 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  base: "/J27/",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"]
 })
